0bfdb5cdaa6e1033a521868a82d7bc0c1ee3e63c
ErikaCetera <ceteraerika@gmail.com>
2024-11-19 19:23:29 +0100

    feat: Milestone 3

---
+++ js/script.js
@@ -0,0 +1,28 @@
+
+const rowElem = document.querySelector(".row")
+
+const printPhotos = () =>{
+    let cards ="";
+    photos.forEach(curPhoto => {
+        cards += `
+        <div class="col-4">
+                    <div class="card p-3">
+                        <img class="pin" src="./img/pin.svg" alt="">
+                        <img src="${curPhoto.url}" alt${curPhoto.title}
+                        <p class="description pt-3">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Veritatis hic</p>
+                    </div>
+                </div>
+              `
+    });
+     rowElem.innerHTML = cards;
+}
+
+let photos = [];
+for(i = 0; i < 6; i++){
+axios.get("https://jsonplaceholder.typicode.com/photos?_limit=6").then(resp => {
+   photos = resp.data;
+   printPhotos();
+   
+   
+});
+};
